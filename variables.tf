33d33b6c798bcd1444d3636b6051f0a9c571479e
jax wood <jax.wood.m@gmail.com>
2025-04-18 22:46:40 +0300

    added files in the root

---
+++ variables.tf
@@ -0,0 +1,13 @@
+##########################
+# Azure Tenant Information
+##########################
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
